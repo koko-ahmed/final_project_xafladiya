466bc38371a57d75907c24f2c9efdc4007a99bcf
Your Name <you@example.com>
2025-06-15 19:17:45 +0300

    event style changed

--- get_hash.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Simple script to generate a password hash
-
-$password = 'Mama.123'; // The plain text password
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-echo "The hashed password for 'Mama.123' is: <br>";
-echo htmlspecialchars($hashed_password); // Display the hash
-?> 
